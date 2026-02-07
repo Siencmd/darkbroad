1d6d6a17996292efa8a18613dee53609425b7b26
Siencmd <sinsender0911@gmail.com>
2026-02-08 02:25:46 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,16 @@
-// Import modular Firebase API
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { 
+  getAuth 
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
-// Your Firebase config
+import { 
+  getFirestore, 
+  doc, 
+  updateDoc, 
+  arrayUnion, 
+  serverTimestamp 
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBBnAXVwZJXc1hf7Q5KNnRTBjGC1MOoy9c",
   authDomain: "darksbord.firebaseapp.com",
@@ -12,9 +20,9 @@ const firebaseConfig = {
   appId: "1:568179041668:web:075eb56e6808fffa8469d1"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
-// Export auth to use in other scripts
-export { auth };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export { doc, updateDoc, arrayUnion, serverTimestamp };
