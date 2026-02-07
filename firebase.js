ae99fa40f25126da415500f68c51a86da974ed88
Siencmd <sinsender0911@gmail.com>
2026-02-08 03:27:14 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -12,12 +12,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBBnAXVwZJXc1hf7Q5KNnRTBjGC1MOoy9c",
-  authDomain: "darksbord.firebaseapp.com",
-  projectId: "darksbord",
-  storageBucket: "darksbord.firebasestorage.app",
-  messagingSenderId: "568179041668",
-  appId: "1:568179041668:web:075eb56e6808fffa8469d1"
+  apiKey: "AIzaSyAx3BTNnEDw4AXEkk5GVR5NLeGjsfVAmlA",
+  authDomain: "darkbroads.firebaseapp.com",
+  projectId: "darkbroads",
+  storageBucket: "darkbroads.firebasestorage.app",
+  messagingSenderId: "271741371558",
+  appId: "1:271741371558:web:45372043bfe9d3205ee067",
+  measurementId: "G-SLEM50XYCK"
 };
 
 const app = initializeApp(firebaseConfig);
